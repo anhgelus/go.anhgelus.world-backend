52c7797ff0277a3736ebf5ad829cd9871824c82d
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@proton.me>
2023-08-03 13:14:24 +0200

    feat(handler): redirect with config

--- src/handler.go
+++ src/handler.go
@@ -1,10 +1,24 @@
 package src
 
 import (
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
 func HandleBasic(w http.ResponseWriter, r *http.Request) {
-	//vars := mux.Vars(r)
-	http.Redirect(w, r, "", http.StatusSeeOther)
+	vars := mux.Vars(r)
+	id := vars["id"]
+	var redirection *Redirect
+	for _, red := range cfg.Redirections {
+		if id == red.Id {
+			redirection = &red
+		}
+	}
+	if redirection == nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	redirect := vars["redirect"]
+	loc := Location{redirect, redirection.Path.generateOrigin()}
+	http.Redirect(w, r, loc.generateUrl(), http.StatusSeeOther)
 }
