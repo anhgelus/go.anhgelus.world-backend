c8ab08fa2a39b3d6e64ea90c440dd7ddef4116e2
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@proton.me>
2023-08-03 12:59:27 +0200

    feat(config): config struct

--- src/config.go
+++ src/config.go
@@ -1,1 +1,27 @@
 package src
+
+import "fmt"
+
+type Config struct {
+	Redirections []Redirect
+}
+
+type Redirect struct {
+	Id   string
+	Path Path
+}
+
+type Path string
+
+type Location struct {
+	Origin string
+	Uri    string
+}
+
+func (p *Path) generateOrigin() string {
+	return fmt.Sprintf("https://%s.anhgelus.world/", *p)
+}
+
+func (l *Location) generateUrl() string {
+	return l.Origin + l.Uri
+}
